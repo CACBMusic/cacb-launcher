d1f13227dbe69d1d70f5b898c0e414628a235f4d
CACBMusic <CACBMusica@gmail.com>
2026-05-01 01:22:31 -0400

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,16 +2,15 @@ import { createClient } from "@supabase/supabase-js";
 
 export default async function handler(req, res) {
 
-  // 👇 IMPORTANTE: solo permitir POST
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(200).json({ message: "Use POST" });
   }
 
   try {
 
     const supabase = createClient(
-      NEXT_PUBLIC_SUPABASE_URL=https://yoekpsiaqnwmcegrgtjr.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_cQ0zQVB7rN_vT0nf66fqnA_OPEDe023
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY
     );
 
     const { email, password } = req.body;
@@ -34,7 +33,7 @@ NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_cQ0zQVB7rN_vT0nf66fqnA_OPEDe
     });
 
   } catch (err) {
-    console.error("SERVER ERROR:", err);
-    return res.status(500).json({ error: "Internal server error" });
+    console.error("ERROR:", err);
+    return res.status(500).json({ error: "Server error" });
   }
 }
